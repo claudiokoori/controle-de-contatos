9d3ee2e00bc8536a8d87a92fee654c7315ca2e11
claudiokoori <claudiojrkoori@gmail.com>
2024-03-10 19:55:56 -0300

    Tela Para Alterar Senha

--- ControleDeContatos/Controllers/AlterarSenhaController.cs
+++ ControleDeContatos/Controllers/AlterarSenhaController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Helper;
+using ControleDeContatos.Models;
+using ControleDeContatos.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeContatos.Controllers
 {
     public class AlterarSenhaController : Controller
     {
+        private readonly IUsuarioRepository _usuarioRepositorio;
+        private readonly ISessao _sessao;
+
+        public AlterarSenhaController(IUsuarioRepository usuarioRepositorio,
+                                      ISessao sessao)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _sessao = sessao;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                UsuarioModel usuarioLogado = _sessao.BuscarSessaoDoUsuario();
+                alterarSenhaModel.Id = usuarioLogado.Id;
+
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.AlterarSenha(alterarSenhaModel);
+                    TempData["MensagemSucesso"] = "Senha alterada com sucesso!";
+                    return View("Index", alterarSenhaModel);
+                }
+
+                return View("Index", alterarSenhaModel);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos alterar sua senha, tente novamante, detalhe do erro: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
         }
     }
 }
