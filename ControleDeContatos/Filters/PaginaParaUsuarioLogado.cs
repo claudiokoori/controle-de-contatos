235215c398c04cf6bbea787822b0e9d597996454
claudiokoori <claudiojrkoori@gmail.com>
2024-03-10 15:47:56 -0300

    Controle de Perfil

---
+++ ControleDeContatos/Filters/PaginaParaUsuarioLogado.cs
@@ -0,0 +1,30 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace ControleDeContatos.Filters
+{
+    public class PaginaParaUsuarioLogado : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string sessaoUsario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if(string.IsNullOrEmpty(sessaoUsario) )
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login"}, { "action", "Index"} });
+            } else
+            {
+                UsuarioModel usuario = JsonSerializer.Deserialize<UsuarioModel>(sessaoUsario);
+
+                if(usuario == null )
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ ControleDeContatos/Filters/PaginaRestritaSomenteAdmin.cs
@@ -0,0 +1,35 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace ControleDeContatos.Filters
+{
+    public class PaginaRestritaSomenteAdmin : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string sessaoUsario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if(string.IsNullOrEmpty(sessaoUsario) )
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login"}, { "action", "Index"} });
+            } else
+            {
+                UsuarioModel usuario = JsonSerializer.Deserialize<UsuarioModel>(sessaoUsario);
+
+                if(usuario == null )
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                }
+
+                if (usuario.Perfil != Enums.PerfilEnum.Admin)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Restrito" }, { "action", "Index" } });
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
